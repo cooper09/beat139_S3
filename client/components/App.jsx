464edba8f763c7c9c32125755ac350d3b24340bb
cooper09 <cooperatkmba@gmail.com>
2019-05-05 17:21:40 -0400

    simple hello world

---
+++ client/components/App.jsx
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+
+
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div style={{textAlign: 'center'}}>
+        <h1>AWS to Go</h1>
+      </div>);
+  }
+}
